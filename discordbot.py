b44b3a8f53e2cd096c17e03902757e0ea5383063
MinKyu <96555576+CaruErun@users.noreply.github.com>
2023-03-07 22:59:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,9 +2,15 @@ from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
 from dotenv import load_dotenv
+import time
+import math
+from datetime import datetime, timedelta
 import os
 load_dotenv()
 
+global startTime
+global endTime
+startTime=""
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
@@ -25,6 +31,21 @@ async def on_message(message):
     if message.content.startswith(f'{PREFIX}hello'):
         await message.channel.send('Hello!')
 
+    if message.content.startswith(f'{PREFIX}start'):
+        global startTime
+        startTime = datetime.now()
+        await message.channel.send("시작시간 : "+startTime.strftime("%H시 %M분 %S초"))
+        
+    if message.content.startswith(f'{PREFIX}end'):
+        global endTime
+        global startTime
+        endTime = datetime.now()
+        if(startTime=="") :
+            await message.channel.send('#start를 먼저 진행해주세요')
+        else :
+            minusTime = endTime - startTime
+            startTime=""
+            await message.channel.send('종료시간 : '+endTime.strftime("%H시 %M분 %S초")+'\n총 시간 : '+ str(datetime.utcfromtimestamp(minusTime.total_seconds()).strftime("%H시 %M분 %S초")))             
 
 try:
     client.run(TOKEN)
